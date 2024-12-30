e75f63de0802a3198378750698f828b319df4c83
KatariinaTomann <katariina.tomann@tktk.ee>
2024-12-30 20:34:07 +0200

    Add project files.

---
+++ RKE132 - Week 9 - Andmete failidesse salvestamine/Program.cs
@@ -0,0 +1,56 @@
+﻿string folderPath = @"C:\Data";
+string fileName = "shoppingList.txt";
+string filePath = Path.Combine(folderPath, fileName);
+List<string> myShoppingList = new List<string>();
+
+if (File.Exists(filePath))
+{
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+else
+{
+    File.Create(filePath).Close();
+    Console.WriteLine($"File {fileName} has been created.");
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+
+
+
+List<string> myShoppingList = GetItemsFromUser();
+ShowItemsFromList(myShoppingList);
+
+static List<string> GetItemsFromUser()
+{
+    List<string>myShoppingList= new List<string>();
+
+    while (true)
+    {
+    Console.WriteLine("Add an item (add)/ Exit (exit):");
+    string userChoice = Console.ReadLine();
+
+    if (userChoice == "add")
+    {
+        Console.WriteLine("Enter an item:");
+        string userItem = Console.ReadLine();
+        myShoppingList.Add(userItem);
+
+    }
+    else
+    {
+        Console.WriteLine("Bye!");
+        break;
+    }
+}
+return myShoppingList
+}
+
+Console.Clear();
+
+int i = 1;
+foreach (string item in myShoppingList)
+{
+    Console.WriteLine($"{i}. {item}");
+    i++
+}
